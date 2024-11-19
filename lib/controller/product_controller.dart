4017d6e4ce881f6cfd7b06891af1bdaa90d9057c
SingHuyLeang <leang6525@gmail.com>
2024-11-19 13:19:11 +0700

    Build model & controller

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,41 @@
+import '../model/product_model.dart';
+
+class ProductController {
+  List<Product> products = [
+    Product(
+      id: 1,
+      name: "Expresso",
+      sizes: ["S","M","L"],
+      price: [1.90, 2.90, 3.15],
+      image: "assets\expresso.jpg",
+    ),
+    Product(
+      id: 2,
+      name: "Caramel Frappucino",
+      sizes: ["S","M","L"],
+      price: [2.90, 3.10, 3.75],
+      image: "assets/Caramel Frappucino.jpg",
+    ),
+    Product(
+      id: 3,
+      name: "Frape Chocolate",
+      sizes: ["S","M","L"],
+      price: [2.80, 3.00, 3.50],
+      image: "assets\Frape Chocolate.png",
+    ),
+    Product(
+      id: 4,
+      name: "Ice Coffee",
+      sizes: ["S","M","L"],
+      price: [2.70, 3.00, 3.40],
+      image: "assets\Ice Coffee.jpg",
+    ),
+    Product(
+      id: 5,
+      name: "Mixed Black Coffee",
+      sizes: ["S","M","L"],
+      price: [3.50, 4.00, 4.50],
+      image: "assets\Mixed Black Coffee.jpg",
+    ),
+  ];
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
+import '../controller/product_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +12,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final productCtrl = ProductController();
+  
+  @override
+  void initState() {
+    log(productCtrl.products.toString());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
