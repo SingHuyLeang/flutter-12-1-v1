b37305267785ea6887a51e0ce3e205a9810d6e2a
SingHuyLeang <leang6525@gmail.com>
2024-11-21 13:30:59 +0700

    Muy Muy

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -18,29 +18,50 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 32,
-            left: 16,
-            right: 16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AppText(
-                "WELCOME TO ORIF COFFEE",
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-                color: const Color(0xFF4C7766),
-              ),
-              const SizedBox(height: 14),
-              AppText(
-                "Find your favorite & take your order",
-                fontSize: 14,
-              ),
-              const SizedBox(height: 32),
-              AppTextField(),
-            ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 32,
+              left: 16,
+              right: 16,
+              bottom: 32,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppText(
+                  "WELCOME TO ORIF COFFEE",
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xFF4C7766),
+                ),
+                const SizedBox(height: 14),
+                AppText(
+                  "Find your favorite & take your order",
+                  fontSize: 14,
+                ),
+                const SizedBox(height: 32),
+                AppTextField(),
+                const SizedBox(height: 32),
+                AppText("All"),
+                const SizedBox(height: 22),
+                GridView.builder(
+                  itemCount: productCtrl.products.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 3,
+                    childAspectRatio: 9 / 16,
+                  ),
+                  itemBuilder: (context, index) => Container(
+                    width: double.infinity,
+                    color: Colors.yellow[300],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
