948db37011ba417d1101604f978e89ef6e820341
SingHuyLeang <leang6525@gmail.com>
2024-12-26 13:49:13 +0700

    26/12/2024

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:note_app/ui/colors/colors.dart';
+import 'package:note_app/ui/typography/typographies.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: Text(
+          'Note',
+          style: typographies.display(
+            fontWeight: FontWeight.w700,
+            color: AppColors.dark,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: SvgPicture.asset('assets/icons/menu-two-line.svg'),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 35,
+              padding: const EdgeInsets.only(right: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: AppColors.primary.withOpacity(0.05),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: TextField(
+                style: typographies.paragraph(color: AppColors.dark),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.only(bottom: 13),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SvgPicture.asset(
+                      'assets/icons/search.svg',
+                    ),
+                  ),
+                  hintText: "Search",
+                  hintStyle: typographies.paragraph(
+                    color: AppColors.dark.withOpacity(0.6),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/typography/typographies.dart
+++ lib/ui/typography/typographies.dart
@@ -1,55 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/ui/colors/colors.dart';
+
+/// <b>Typographies</b> <br/>
+/// It's for use with Text
 
 abstract class Typographies {
-  TextStyle display({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow});
-  TextStyle title({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow});
-  TextStyle paragraph({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow});
-  TextStyle label({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow});
+  TextStyle display({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  });
+  TextStyle title({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  });
+  TextStyle paragraph({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  });
+  TextStyle label({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  });
 }
+
+final Typographies typographies = TypographiesImpl();
 
 class TypographiesImpl implements Typographies {
   final String _fontFamily = "poppins";
 
   @override
-  TextStyle display({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow}) {
+  TextStyle display({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  }) {
     return TextStyle(
       fontFamily: _fontFamily,
-      color: color,
+      color: color ?? AppColors.dark,
       fontWeight: fontWeight,
-      fontSize: 36,
+      fontSize: 20,
     );
   }
-  
+
   @override
-  TextStyle label({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow}) {
+  TextStyle label({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  }) {
     return TextStyle(
       fontFamily: _fontFamily,
-      color: color,
-      fontWeight: fontWeight,
-      fontSize: 10,
-    );
-    
-  }
-  
-  @override
-  TextStyle paragraph({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow}) {
-    return TextStyle(
-      fontFamily: _fontFamily,
-      color: color,
+      color: color ?? AppColors.dark,
       fontWeight: fontWeight,
       fontSize: 12,
     );
-    
   }
-  
+
   @override
-  TextStyle title({Color? color, FontWeight? fontWeight, int? maxLine, TextOverflow? overflow}) {
+  TextStyle paragraph({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  }) {
     return TextStyle(
       fontFamily: _fontFamily,
-      color: color,
+      color: color ?? AppColors.dark,
       fontWeight: fontWeight,
       fontSize: 14,
     );
-    
+  }
+
+  @override
+  TextStyle title({
+    Color? color,
+    FontWeight? fontWeight,
+    int? maxLine,
+    TextOverflow? overflow,
+  }) {
+    return TextStyle(
+      fontFamily: _fontFamily,
+      color: color ?? AppColors.dark,
+      fontWeight: fontWeight,
+      fontSize: 18,
+    );
   }
 }
